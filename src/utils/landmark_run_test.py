7fce3538c8c8a176f4627e6560c3daa1aedc9e95
Nikita Denisov <nikita160412@yandex.ru>
2024-05-01 21:01:23 +0000

    Benchmark mistral on landmark passkey

--- src/utils/landmark_run_test.py
+++ src/utils/landmark_run_test.py
@@ -23,8 +23,11 @@ import json
 from collections import defaultdict
 import sys
 
+from definitions import *
+
 
 llama_weights_7b_base = "meta-llama/Llama-2-7b-hf"
+mistral_7b = "mistralai/Mistral-7B-v0.1"
 # llama_weights_7b_base = "/llama_weights/7B_hf/"
 llama_weights_7b_tuned = "/llama-redpajama-mem-15000-with-mem/"
 cache_path = "./hf-cache/"
@@ -62,41 +65,68 @@ def make_llama_base_pipe():
 
 llama_base_pipe = make_llama_base_pipe()
 
-def make_llama_mem_pipe():
-    from llama_mem import LlamaForCausalLM
+def make_casual_llm_base_pipe(repo):
 
-    model = LlamaForCausalLM.from_pretrained(
-        llama_weights_7b_tuned,
-        cache_dir=cache_path,
-        torch_dtype=dtype
+    from transformers import pipeline
+    from transformers import AutoModelForCausalLM, AutoTokenizer
+
+    llm_base = AutoModelForCausalLM.from_pretrained(
+        repo,
+        cache_dir=cache_path
     )
-
-    model.to('cuda:1')
+    
+    llm_base = llm_base.to('cuda')
 
     import transformers
+    
+    tokenizer = AutoTokenizer.from_pretrained(
+        repo,
+        cache_dir=cache_path,
+        use_fast=False,
+    )
 
-    tokenizer = transformers.AutoTokenizer.from_pretrained(
-            llama_weights_7b_tuned,
-            cache_dir=cache_path,
-            model_max_length=model.config.train_context_length,
-            padding_side="right",
-            use_fast=False,
-        )
-    mem_id = tokenizer.convert_tokens_to_ids("<landmark>")
-    model.set_mem_id(mem_id)
-    from transformers import pipeline
-    llama_mem_pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=model.device,
-                              offload_cache_to_cpu=use_flash, use_flash=use_flash, 
-                              cache_top_k=top_k)
-    return llama_mem_pipe
+    llm_base_pipe = pipeline("text-generation", model=llm_base, tokenizer=tokenizer, device=llm_base.device)
+    return llm_base_pipe
+
+
+
+mistral_base_pipe = make_casual_llm_base_pipe(mistral_7b)
+
+# def make_llama_mem_pipe():
+#     from llama_mem import LlamaForCausalLM
+
+#     model = LlamaForCausalLM.from_pretrained(
+#         llama_weights_7b_tuned,
+#         cache_dir=cache_path,
+#         torch_dtype=dtype
+#     )
+
+#     model.to('cuda:1')
+
+#     import transformers
+
+#     tokenizer = transformers.AutoTokenizer.from_pretrained(
+#             llama_weights_7b_tuned,
+#             cache_dir=cache_path,
+#             model_max_length=model.config.train_context_length,
+#             padding_side="right",
+#             use_fast=False,
+#         )
+#     mem_id = tokenizer.convert_tokens_to_ids("<landmark>")
+#     model.set_mem_id(mem_id)
+#     from transformers import pipeline
+#     llama_mem_pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=model.device,
+#                               offload_cache_to_cpu=use_flash, use_flash=use_flash, 
+#                               cache_top_k=top_k)
+#     return llama_mem_pipe
 
 
 # llama_mem_pipe = make_llama_mem_pipe()
 
 
 
-
-pipes = {"base": llama_base_pipe}
+pipes = {MISTRAL_7B: mistral_base_pipe}
+# pipes = {"base": llama_base_pipe}
 # pipes = {"base": llama_base_pipe, "mem": llama_mem_pipe}
 
 
@@ -133,57 +163,67 @@ def test_model(prompt_text, pass_key, model_name):
         pass_key = int(re.search(r'\d+', response).group())
     except:
         pass_key = response[:20]
+        
+    del response
+    torch.cuda.empty_cache()
+    import gc
+    gc.collect()
     
     return pass_key
 
 
-# n_values = [12000, 14000, 18000, 20000, 25000, 38000]
-n_values = [0, 100, 500, 1000, 5000, 8000, 10000, 12000, 14000, 18000, 20000, 25000, 38000]
-num_tests = 50
-models = ["base"]
-# models = ["base", "mem"]
-accuracies = {x: [] for x in models}
-individual_results = {x: [] for x in models}
+def test_passkey_full(models, n_values=None):
+    print(f"models = {models}")
+    if n_values is None:
+        n_values = [0, 100, 500, 1000, 5000, 8000, 10000, 12000, 14000, 18000, 20000, 25000, 38000]
+    num_tests = 50
+    # models = ["base", "mem"]
+    accuracies = {x: [] for x in models}
+    individual_results = {x: [] for x in models}
 
-num_tokens_in_prompt = defaultdict(list)
-
-for n in tqdm(n_values, desc="n_values..."):
+    num_tokens_in_prompt = defaultdict(list)
     
-    correct_count = {x: 0 for x in models}
+    out_dir = Path("./landmark_passkey_results")
+    out_dir.mkdir(exist_ok=True, parents=True)
+
+    for n in tqdm(n_values, desc="n_values..."):
+
+        correct_count = {x: 0 for x in models}
+
+        n_results = {x: [] for x in models}
+        for i in trange(num_tests, desc="Tests..."):
+            # print(f"\nRunning test {i + 1}/{num_tests} for n = {n}...")
+            prompt_text, pass_key = generate_prompt(n)
+
+
+
+            for model_name in models:
+                if pipes[model_name] is None:
+                    continue
+                num_tokens = len(pipes[model_name].tokenizer.encode(prompt_text))
+                num_tokens_in_prompt[f"{model_name}_{n}"].append(num_tokens)
+
+                # print("Number of tokens in this prompt: ", num_tokens)
+                model_output = test_model(prompt_text, pass_key, model_name)
+                # print(f"Expected number in the prompt: {pass_key}, {model_name} output: {model_output}")
+
+                if pass_key == model_output:
+                    correct_count[model_name] += 1
+                    n_results[model_name].append(1)
+                    # print("Success!")
+                else:
+                    n_results[model_name].append(0)
+                    # print("Fail.")
+
+        for model in models:
+            accuracy = (correct_count[model] / num_tests) * 100
+            with open(out_dir.joinpath(f"{model}_{n}.json"), "w") as f:
+                json.dump({"accuracy": accuracy}, f, ensure_ascii=False, indent=2)
+            print(f"Accuracy {model} for n = {n}: {accuracy}%")
+            accuracies[model].append(accuracy)
+            individual_results[model].append(n_results)
+
     
-    n_results = {x: [] for x in models}
-    for i in trange(num_tests, desc="Tests..."):
-        # print(f"\nRunning test {i + 1}/{num_tests} for n = {n}...")
-        prompt_text, pass_key = generate_prompt(n)
-        
-        
-        
-        for model_name in models:
-            if pipes[model_name] is None:
-                continue
-            num_tokens = len(pipes[model_name].tokenizer.encode(prompt_text))
-            num_tokens_in_prompt[n].append(num_tokens)
-            continue
-
-            # print("Number of tokens in this prompt: ", num_tokens)
-            model_output = test_model(prompt_text, pass_key, model_name)
-            # print(f"Expected number in the prompt: {pass_key}, {model_name} output: {model_output}")
-
-            if pass_key == model_output:
-                correct_count[model_name] += 1
-                n_results[model_name].append(1)
-                # print("Success!")
-            else:
-                n_results[model_name].append(0)
-                # print("Fail.")
-    
-    for model in models:
-        accuracy = (correct_count[model] / num_tests) * 100
-        # with open(f"./llama-2-7b/{n}.json", "w") as f:
-        #     json.dump({"accuracy": accuracy}, f, ensure_ascii=False, indent=2)
-        print(f"Accuracy {model} for n = {n}: {accuracy}%")
-        accuracies[model].append(accuracy)
-        individual_results[model].append(n_results)
-
-print("???")
-print(num_tokens_in_prompt, file=sys.stderr)
+    # print(num_tokens_in_prompt, file=sys.stderr)
+    # with open(out_dir.joinpath("num_tokens_in_prompt.json"), "w") as f:
+    #     json.dump(num_tokens_in_prompt, f, ensure_ascii=False, indent=2)
